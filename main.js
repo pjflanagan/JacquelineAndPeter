76cd5ce8a7652272eb003b6cef572d7eed43ebe2
pflanagan <pflanagan@grubhub.com>
2024-01-26 13:03:34 -0500

    like this theme

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 
-(function() {
+(function () {
+  const PAGE_CENTER = $(window).innerHeight() / 2;
+
   $('.sidebar').on('scroll', () => {
-    $('.slideshow-marker').each(() => {
-      const top = $(this).offset().top;
-      console.log(top);
-    })
+    let closestElementScrollDistance = Infinity;
+    let closestElement;
+
+    $('.slideshow-marker').each(function () {
+      const scrollDistance = $(this).offset().top - PAGE_CENTER;
+
+      // if we haven't reached there yet, ignore
+      if (scrollDistance > 0) {
+        return;
+      }
+
+      // otherwise see if this is the closest scroll point
+      if (Math.abs(scrollDistance) < closestElementScrollDistance) {
+        closestElementScrollDistance = Math.abs(scrollDistance);
+        closestElement = this;
+      }
+
+    });
+
+    // if we found a closest element, then set the background to what it says
+    if (closestElement) {
+      const slideshowImage = $(closestElement).attr('data-slideshowImage');
+      $('#slideshow').css({
+        backgroundImage: `url(${slideshowImage})`,
+        // backgroundSize: 'cover',
+        // backgroundPosition: 'center'
+      })
+    }
+
   });
 })();
